ea024ffc74ca6d6513838be0c240b9b2716ca2a4
Joseli Ramos <josielisouzaderamos714@gmail.com>
2024-08-05 10:10:17 -0300

    Polimorfismo na prática

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,19 @@
 package org.example;
 
+import com.bank.model.Account;
+import com.bank.model.SavingsAccount;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Account x = new Account(1020, "Alex", 1000.0);
+        Account y = new SavingsAccount(1023, "Maria", 1000.0, 0.01);
+
+        //Polimorfismo --> Poli = muitos; morfismo = forma / variáveis de mesmo tipo se comportando de diferentes formas
+
+        x.withdraw(50.0);
+        y.withdraw(50.0);
+
+        System.out.println(x.getBalance() + "\n" + y.getBalance());
     }
 }
