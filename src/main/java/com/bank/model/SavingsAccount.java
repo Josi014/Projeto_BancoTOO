d53776ce384f27bc50412d1627e9bcc8107c175f
Joseli Ramos <josielisouzaderamos714@gmail.com>
2024-08-05 08:01:00 -0300

    Uso da sobrescrita de método no método withdraw

--- src/main/java/com/bank/model/SavingsAccount.java
+++ src/main/java/com/bank/model/SavingsAccount.java
@@ -23,4 +23,9 @@ public class SavingsAccount extends Account{
     public void updateBalance(){
         balance += balance * interestRate;
     }
+
+    @Override
+    public void withdraw(double amount){
+        balance -= amount;
+    }
 }
